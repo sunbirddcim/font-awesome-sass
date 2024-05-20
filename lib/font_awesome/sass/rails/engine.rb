957367e2a5842fb232196d48db1df449c763fcd0
Jason Herr <jason.herr@sunbirddcim.com>
2024-05-20 10:34:30 -0400

    Refactor paths so dartsass + sprockets will find the manifest and other files

--- lib/font_awesome/sass/rails/engine.rb
+++ lib/font_awesome/sass/rails/engine.rb
@@ -3,7 +3,7 @@ module FontAwesome
     module Rails
       class Engine < ::Rails::Engine
         initializer 'font-awesome-sass.assets.precompile', group: :all do |app|
-          app.config.assets.paths << root.join('assets', 'fonts').to_s
+          app.config.assets.paths << root.join('app', 'assets', 'fonts').to_s
 
           %w[ttf woff2].each do |ext|
             app.config.assets.precompile << "font-awesome/fa-brands-400.#{ext}"
